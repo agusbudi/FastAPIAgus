2275ec813c421b3eb7ae74008cc63aa628cab83a
agusbudi <agusbr.br@gmail.com>
2022-10-11 20:06:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ model = pickle.load(model_file, encoding='bytes')
 class Msg(BaseModel):
     msg: str
 
+class Req(BaseModel):
+    age: int
+    sex: str
+    smoker: str
+
 @app.get("/")
 async def root():
     return {"message": "Hello World. Welcome to FastAPI!"}
@@ -35,28 +40,30 @@ async def predict(path_id: int):
      return {"message":  f"This is /predict/{path_id} endpoint, use post request to retrieve result"}
     
 @app.post("/predict")
-async def predict():
+async def predict(requess: Req)
     '''
     Predict the insurance cost based on user inputs
     and render the result to the html page
     '''
-    age, sex, smoker = [x for x in request.form.values()]
+    return requess
+#     age = requess.age
+#     age, sex, smoker = [x for x in request.form.values()]
 
-    data = []
+#     data = []
 
-    data.append(int(age))
-    if sex == 'Laki-laki':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
+#     data.append(int(age))
+#     if sex == 'Laki-laki':
+#         data.extend([0, 1])
+#     else:
+#         data.extend([1, 0])
 
-    if smoker == 'Ya':
-        data.extend([0, 1])
-    else:
-        data.extend([1, 0])
+#     if smoker == 'Ya':
+#         data.extend([0, 1])
+#     else:
+#         data.extend([1, 0])
     
-    prediction = model.predict([data])
-    output = round(prediction[0], 2)
+#     prediction = model.predict([data])
+#     output = round(prediction[0], 2)
     
-    return {"it works"}
+#    return {"it works"}
 #     #return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
