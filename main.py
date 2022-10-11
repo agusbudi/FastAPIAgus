c58d4b4a77f1da1ce30b97a6d98868b9785bbc2d
agusbudi <agusbr.br@gmail.com>
2022-10-11 19:27:41 +0700

    add machine learning

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
+model_file = open('model.pkl', 'rb')
+model = pickle.load(model_file, encoding='bytes')
+
 class Msg(BaseModel):
     msg: str
-
 
 @app.get("/")
 async def root():
@@ -25,3 +28,31 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+
+@app.post("/predict")
+async def predict():
+    '''
+    Predict the insurance cost based on user inputs
+    and render the result to the html page
+    '''
+    age, sex, smoker = [x for x in request.form.values()]
+
+    data = []
+
+    data.append(int(age))
+    if sex == 'Laki-laki':
+        data.extend([0, 1])
+    else:
+        data.extend([1, 0])
+
+    if smoker == 'Ya':
+        data.extend([0, 1])
+    else:
+        data.extend([1, 0])
+    
+    prediction = model.predict([data])
+    output = round(prediction[0], 2)
+    
+    return {"it works"}
+    #return render_template('index.html', insurance_cost=output, age=age, sex=sex, smoker=smoker)
